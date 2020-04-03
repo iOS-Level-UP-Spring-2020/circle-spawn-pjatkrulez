9507d55b8a6f84d2e3365323a0c87f67bb6e96ec
Jakub <kuba.dzien98@gmai.com>
2020-04-03 14:31:31 +0200

    creating circle

--- CircleSpawn/CircleSpawn/CircleSpawnController.swift
+++ CircleSpawn/CircleSpawn/CircleSpawnController.swift
@@ -7,5 +7,21 @@ class CircleSpawnController: UIViewController {
 	override func loadView() {
 		view = UIView()
 		view.backgroundColor = .white
+        
+        let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.checkAction))
+        gesture.numberOfTapsRequired = 2
+        self.view.addGestureRecognizer(gesture)
 	}
+
+    @objc func checkAction(sender : UITapGestureRecognizer) {
+        createCircle(tap: sender)
+     }
+    
+    func createCircle(tap: UITapGestureRecognizer){
+        let point = tap.location(in: self.view)
+        let circleView : UIView = UIView(frame: CGRect(x: point.x, y: point.y, width: 100, height: 100))
+        circleView.backgroundColor = UIColor.randomBrightColor()
+        circleView.layer.cornerRadius = 50
+        self.view.addSubview(circleView)
+    }
 }
