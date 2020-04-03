1dd3fd974a6e4f49aec59ebd9dc38223d0bba199
Jan Biernacki <jas.biernacki@gmail.com>
2020-04-03 15:59:42 +0200

    circle movement after long press

--- CircleSpawn/CircleSpawn/CircleSpawnController.swift
+++ CircleSpawn/CircleSpawn/CircleSpawnController.swift
@@ -9,13 +9,19 @@ class CircleSpawnController: UIViewController {
 		view.backgroundColor = .white
         
         let gesture = UITapGestureRecognizer(target: self, action:  #selector(self.checkAction))
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(self.checkLongPress))
         gesture.numberOfTapsRequired = 2
         self.view.addGestureRecognizer(gesture)
+        self.view.addGestureRecognizer(longPress)
 	}
 
     @objc func checkAction(sender : UITapGestureRecognizer) {
         createCircle(tap: sender)
      }
+    
+    @objc func checkLongPress(sender: UILongPressGestureRecognizer) {
+        moveCircle(press: sender)
+    }
     
     func createCircle(tap: UITapGestureRecognizer){
         let point = tap.location(in: self.view)
@@ -24,4 +30,18 @@ class CircleSpawnController: UIViewController {
         circleView.layer.cornerRadius = 50
         self.view.addSubview(circleView)
     }
+    
+    func moveCircle(press: UILongPressGestureRecognizer){
+        let point = press.location(in: self.view)
+        let filteredSubviews = view.subviews.filter { subView -> Bool in
+          return subView.frame.contains(point)
+        }
+        guard let subviewPressed = filteredSubviews.first else {
+          return
+        }
+        subviewPressed.center = point
+    }
+    
+    
+    
 }
